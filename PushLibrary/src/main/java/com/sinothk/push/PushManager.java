7c571b9f4dbb60aa70e54e226bbe55962ad8abee
SINOTHK\LiangYT <381518188@qq.com>
2018-01-09 10:53:35 +0800

    2018年1月9日 10:53:31

---
+++ PushLibrary/src/main/java/com/sinothk/push/PushManager.java
@@ -0,0 +1,12 @@
+package com.sinothk.push;
+
+/**
+ * Created by 梁玉涛 on 2018/1/9.
+ */
+
+public class PushManager {
+
+    public static void init() {
+    }
+
+}
--- app/src/main/java/com/sinothk/push/demo/MainActivity.java
+++ app/src/main/java/com/sinothk/push/demo/MainActivity.java
@@ -3,11 +3,15 @@ package com.sinothk.push.demo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.sinothk.push.PushManager;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        PushManager.init();
     }
 }
